06d82c18494ee20b23c661fd1734285e9d7754f2
techwithken <ti664469@gmail.com>
2024-06-24 21:02:25 +0100

    Got External Help in fintuning the codebase and making it more readable and scalable

--- todoList.dart
+++ todoList.dart
@@ -1,4 +1,4 @@
-/*This code base is a program used to imitate the working principle of a todo list 
+/* This code base is a program used to imitate the working principle of a todo list 
 where individual or users of the app can perform the following operations
 
 1. Add a new task
@@ -9,102 +9,119 @@ where individual or users of the app can perform the following operations
 6. View all completed tasks
 7. View all uncompleted tasks
 8. Exit the app
-
 */
 
 import "dart:io";
 import "dart:async";
 
-var todoList = {
+Map<String, String> todoList = {};
 
-};
-
-addTask(String taskToAdd, Map<dynamic, dynamic> tasks){
+void addTask(String taskToAdd, Map<String, String> tasks) {
   tasks[taskToAdd] = "Uncompleted";
 }
 
-deleteTask(String taskToDelete, Map<dynamic, dynamic> tasks){
+void deleteTask(String taskToDelete, Map<String, String> tasks) {
   tasks.remove(taskToDelete);
 }
 
-completedTask(String taskCompleted, Map<dynamic, dynamic> tasks){
+void completedTask(String taskCompleted, Map<String, String> tasks) {
   tasks[taskCompleted] = "Completed";
 }
 
-uncompletedTask(String taskUncompleted, Map<dynamic, dynamic> tasks){
+void uncompletedTask(String taskUncompleted, Map<String, String> tasks) {
   tasks[taskUncompleted] = "Uncompleted";
 }
 
-viewTask(String taskToView, Map<dynamic, dynamic> tasks){
+void viewTask(String taskToView, Map<String, String> tasks) {
   print(tasks[taskToView]);
 }
 
-viewAllTasks(tasks){
-  for (var task in tasks){
-    print("$task is ${tasks[task]}");
-  }
+void viewAllTasks(Map<String, String> tasks) {
+  tasks.forEach((task, status) {
+    print("$task is $status");
+  });
 }
 
-viewCompletedTasks(tasks){
-  for (var task in tasks){
-    if (tasks[task] == "Completed"){
-      return task;
+List<String> viewCompletedTasks(Map<String, String> tasks) {
+  List<String> completedTasks = [];
+  tasks.forEach((task, status) {
+    if (status == "Completed") {
+      completedTasks.add(task);
     }
-  }
+  });
+  return completedTasks;
 }
 
-viewUncompletedTasks(tasks){
-  for (var task in tasks){
-    if (tasks[task] == "Uncompleted"){
-      return task;
+List<String> viewUncompletedTasks(Map<String, String> tasks) {
+  List<String> uncompletedTasks = [];
+  tasks.forEach((task, status) {
+    if (status == "Uncompleted") {
+      uncompletedTasks.add(task);
     }
-  }
+  });
+  return uncompletedTasks;
 }
-collectTask(){
-  var userTask = stdin.readLineSync();
-  return userTask;
-}
-void main() async{
 
+String? collectTask() {
+  print("Enter the task: ");
+  return stdin.readLineSync();
+}
+
+void main() async {
   print("Please select a choice to select the operation to perform on the todo list ");
   print("Creating TODO LIST........");
   await Future.delayed(Duration(seconds: 3));
-  print(
-    "Pelase Select an operation you would like to perform on the todo list 1: Add new task, 2: Delete Task, 3: Mark Task as Completed" +
-    "4: Mark Task as Uncompleted, 5: View All Tasks, 6: View Completed Tasks, 7: View Uncompleted Tasks, 8: View Task"
-  );
-  var userChoice = stdin.readLineSync();
 
-  if (userChoice == "1"){
-    var theTask = collectTask();
-    addTask(theTask, todoList);
-  }
-  else if (userChoice == "2"){
-    var theTask = collectTask();
-    deleteTask(theTask, todoList);
-  }
-  else if(userChoice == "3"){
-    var theTask = collectTask();
-    completedTask(theTask, todoList);
-  }
-  else if (userChoice == "4"){
-    var theTask = collectTask();
-    uncompletedTask(theTask, todoList);
-  }
-  else if (userChoice == "5"){
-    viewAllTasks(todoList);
-  }
-  else if(userChoice == "6"){
-    print(viewCompletedTasks(todoList));
-  }
-  else if(userChoice == "7"){
-    print(viewUncompletedTasks(todoList));
-  }
-  else if(userChoice == "8"){
-    var theTask = collectTask();
-    viewTask(theTask, todoList);
-  }
-  else{
-    exit(0);
+  while (true) {
+    print(
+      "Please select an operation you would like to perform on the todo list:\n"
+      "1: Add new task\n"
+      "2: Delete Task\n"
+      "3: Mark Task as Completed\n"
+      "4: Mark Task as Uncompleted\n"
+      "5: View All Tasks\n"
+      "6: View Completed Tasks\n"
+      "7: View Uncompleted Tasks\n"
+      "8: View Task\n"
+      "9: Exit"
+    );
+    
+    var userChoice = stdin.readLineSync();
+
+    switch (userChoice) {
+      case "1":
+        var theTask = collectTask();
+        if (theTask != null) addTask(theTask, todoList);
+        break;
+      case "2":
+        var theTask = collectTask();
+        if (theTask != null) deleteTask(theTask, todoList);
+        break;
+      case "3":
+        var theTask = collectTask();
+        if (theTask != null) completedTask(theTask, todoList);
+        break;
+      case "4":
+        var theTask = collectTask();
+        if (theTask != null) uncompletedTask(theTask, todoList);
+        break;
+      case "5":
+        viewAllTasks(todoList);
+        break;
+      case "6":
+        print(viewCompletedTasks(todoList));
+        break;
+      case "7":
+        print(viewUncompletedTasks(todoList));
+        break;
+      case "8":
+        var theTask = collectTask();
+        if (theTask != null) viewTask(theTask, todoList);
+        break;
+      case "9":
+        exit(0);
+      default:
+        print("Invalid choice, please try again.");
+    }
   }
 }
