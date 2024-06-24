7360c9b20723a07877208ee4b4946ee1ca1063b2
techwithken <ti664469@gmail.com>
2024-06-24 17:50:19 +0100

    added functionalities to the application by making all functions work

--- todoList.dart
+++ todoList.dart
@@ -13,6 +13,7 @@ where individual or users of the app can perform the following operations
 */
 
 import "dart:io";
+import "dart:async";
 
 var todoList = {
 
@@ -38,8 +39,10 @@ viewTask(String taskToView, Map<dynamic, dynamic> tasks){
   print(tasks[taskToView]);
 }
 
-viewAllTasks(Map<dynamic, dynamic> tasks){
-  print(tasks);
+viewAllTasks(tasks){
+  for (var task in tasks){
+    print("$task is ${tasks[task]}");
+  }
 }
 
 viewCompletedTasks(tasks){
@@ -57,6 +60,51 @@ viewUncompletedTasks(tasks){
     }
   }
 }
-void main(){
-  print(todoList);
+collectTask(){
+  var userTask = stdin.readLineSync();
+  return userTask;
+}
+void main() async{
+
+  print("Please select a choice to select the operation to perform on the todo list ");
+  print("Creating TODO LIST........");
+  await Future.delayed(Duration(seconds: 3));
+  print(
+    "Pelase Select an operation you would like to perform on the todo list 1: Add new task, 2: Delete Task, 3: Mark Task as Completed" +
+    "4: Mark Task as Uncompleted, 5: View All Tasks, 6: View Completed Tasks, 7: View Uncompleted Tasks, 8: View Task"
+  );
+  var userChoice = stdin.readLineSync();
+
+  if (userChoice == "1"){
+    var theTask = collectTask();
+    addTask(theTask, todoList);
+  }
+  else if (userChoice == "2"){
+    var theTask = collectTask();
+    deleteTask(theTask, todoList);
+  }
+  else if(userChoice == "3"){
+    var theTask = collectTask();
+    completedTask(theTask, todoList);
+  }
+  else if (userChoice == "4"){
+    var theTask = collectTask();
+    uncompletedTask(theTask, todoList);
+  }
+  else if (userChoice == "5"){
+    viewAllTasks(todoList);
+  }
+  else if(userChoice == "6"){
+    print(viewCompletedTasks(todoList));
+  }
+  else if(userChoice == "7"){
+    print(viewUncompletedTasks(todoList));
+  }
+  else if(userChoice == "8"){
+    var theTask = collectTask();
+    viewTask(theTask, todoList);
+  }
+  else{
+    exit(0);
+  }
 }
