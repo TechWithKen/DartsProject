71a25c562cce723592c626d5786858460ebdc8ba
techwithken <ti664469@gmail.com>
2024-06-24 16:33:40 +0100

    Created and initialized a to-do list app

---
+++ todoList.dart
@@ -0,0 +1,14 @@
+/*This code base is a program used to imitate the working principle of a todo list 
+where individual or users of the app can perform the following operations
+
+1. Add a new task
+2. Delete a task
+3. Mark a task as completed
+4. Mark a task as uncompleted
+5. View all Tasks
+6. View all completed tasks
+7. View all uncompleted tasks
+8. Exit the app
+
+*/
+
